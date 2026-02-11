f9a6224bb2fd3bcf2a25360d469387fd5df985fe
Lucas Weiler <lucas.weiler@dynetics.com>
2026-02-11 12:13:54 -0600

    feat: removed prelude into glob import to fix derive paths

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,3 @@
-pub use simple_py_bevy;
-pub mod prelude {
-    pub use simple_py_bevy::*;
-    #[allow(unused_imports)]
-    pub use simple_py_bevy_derive::*;
-}
+pub use simple_py_bevy::*;
+#[allow(unused_imports)]
+pub use simple_py_bevy_derive::*;
